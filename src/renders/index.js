5c0ad677b5b188088556ed4716f42476c13f9c66
raphael.arevalos <raphael.arevalos@external.stellantis.com>
2023-04-16 14:17:47 -0300

    add objetcs

--- src/renders/index.js
+++ src/renders/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const baseData = {
-    title: "MyApp",
     nav_h1: "MyApp"
 }
 
@@ -14,7 +13,9 @@ router.use((req, res, next) => {
   })
 
 router.get('/', (req, res) => {
-    
+    baseData['t1'] = "Sobre o Projeto";
+    baseData['p1'] = "O projeto é sobre o treinamento em EJS. Uma maneira de modelar o html utilizando o nodejs com express."
+
     res.render('pages/index', baseData);
 })
     
